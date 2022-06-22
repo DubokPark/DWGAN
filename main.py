9eb566f8b84a0115ef0479a9b6933033fc640c02
DubokPark <zardmzvll@gmail.com>
2022-06-22 15:04:54 +0900

    test commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import torch
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(torch.cuda.is_available())
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
